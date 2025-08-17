1795f7aa9aa0f02664d405e7bd561841113e3dc5
hassan <hesssan3506@gmail.com>
2025-08-17 15:34:34 +0330

    ++

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('bybit:enforce')->everyMinute();
+        $schedule->command('bybit:lifecycle')->everyMinute();
+        $schedule->command('bybit:sync-sl')->everyMinute();
+
     }
 
     /**
