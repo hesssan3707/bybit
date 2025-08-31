77ec4f8982b4e73e3572e3dea46d1b498f335d27
hassan <hesssan3506@gmail.com>
2025-08-31 10:05:31 +0330

    *******

--- routes/web.php
+++ routes/web.php
@@ -128,7 +128,21 @@ Route::get('/schedule', function() {
     print_r(Artisan::output());
     echo '<br>------------------------------------------ sync sl done --------------------------------------------------<br> ';
     return 'DONE';
-})->middleware('throttle:2');
+    sleep(25);
+    Artisan::call('exchanges:validate-active --force');
+    print_r(Artisan::output());
+    echo '<br>------------------------------------------------------ lifecycle done----------------------------------------- <br>';
+    Artisan::call('futures:lifecycle');
+    print_r(Artisan::output());
+    echo '<br>------------------------------------------------------ lifecycle done----------------------------------------- <br>';
+    Artisan::call('futures:enforce');
+    print_r(Artisan::output());
+    echo '<br>-------------------------------------- enforce done -------------------------------------------------<br>';
+    Artisan::call('futures:sync-sl');
+    print_r(Artisan::output());
+    echo '<br>------------------------------------------ sync sl done --------------------------------------------------<br> ';
+    return 'DONE';
+})->middleware('throttle:3');
 
 // Non-protected utility routes if needed, but it's better to protect them.
 // For simplicity, we can leave these out for now or protect them as well.
